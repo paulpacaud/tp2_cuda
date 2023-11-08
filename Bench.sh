4d560be358028062b254f08d0d9824f1810eab22
Jules Rigaud <Vjules.rigaud@student-cs.fr>
2023-11-08 15:55:35 +0100

    Intsallation bench automatique

--- Bench.sh
+++ Bench.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+RESULTS_G_MP="results_G_MP.txt"
+RESULTS_G_K="results_G_K.txt"
+RESULTS_T_MP="results_T_MP.txt"
+
+> $RESULTS_G_K
+> $RESULTS_G_MP
+> $RESULTS_T_MP
+
 # Backup the original main.h file
 cp main.h main.h.bak
 
 # Loop for X and Y as per your requirement
-for ((X=1; X<=1024; X*=2)); do  # Assuming you want to double X in each iteration for quicker traversal
-    for ((Y=1; Y<=X; Y*=2)); do  # Assuming you want to double Y in each iteration for quicker traversal
+for ((Y=1; Y<=4; Y*=2)); do  # Assuming you want to double X in each iteration for quicker traversal
+    for ((X=1; X*Y<=4; X*=2)); do  # Assuming you want to double Y in each iteration for quicker traversal
 
         # Output X and Y values
-        echo "X: $X, Y: $Y\n"
+        echo "X: $X, Y: $Y"
 
         # Modify main.h with new BLOCK_SIZE_X_K1 and BLOCK_SIZE_Y_K1 values
         sed -i.bak "s/#define BLOCK_SIZE_X_K1[[:space:]]*[0-9]*/#define BLOCK_SIZE_X_K1     $X/" main.h
@@ -18,9 +26,27 @@ for ((X=1; X<=1024; X*=2)); do  # Assuming you want to double X in each iteratio
         make
 
         # Run the executable
-        ./MatrixProduct -gpu-k 1 >> results.txt
+        ./MatrixProduct -gpu-k 1 > raw.txt
 
+        cat raw.txt | grep Elapsed | awk '{ print $5 }' > temp.txt
+        T_MP=$(sed '2d;3d' temp.txt)
+
+        cat raw.txt | grep Gflops | awk '{ print $4 }' > temp.txt
+        G_MP=$(sed '2d' temp.txt)
+        G_K=$(sed '1d' temp.txt)
+
+        rm temp.txt
+        rm raw.txt
+
+        echo -n $T_MP, >> $RESULTS_T_MP
+        echo -n $G_MP, >> $RESULTS_G_MP
+        echo -n $G_K, >> $RESULTS_G_K
+        
+      
     done
+    echo -ne "\n" >> $RESULTS_T_MP
+    echo -ne "\n" >> $RESULTS_G_MP
+    echo -ne "\n" >> $RESULTS_G_K
 done
 
 # Restore the original main.h file
--- main.h
+++ main.h
@@ -12,9 +12,9 @@
 /*-------------------------------------------------------------------------------*/
 
 // Matrix size (side of the 3 matrixes)
-#define SIZE              1024        // To debug
+// #define SIZE              1024        // To debug
 // #define SIZE              1025        // To debug
-// #define SIZE              4096        // To benchmark
+#define SIZE              4096        // To benchmark
 // #define SIZE              4097        // To debug
 
 
@@ -27,8 +27,8 @@
 // Block sizes
 #define BLOCK_SIZE_X_K0     32
 
-#define BLOCK_SIZE_X_K1     2
-#define BLOCK_SIZE_Y_K1     1
+#define BLOCK_SIZE_X_K1     4
+#define BLOCK_SIZE_Y_K1     2
 
 #define BLOCK_SIZE_XY_K2    32
 
